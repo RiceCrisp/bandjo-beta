37fd6696bc8fb702ce29ba5f05814616a9b8c089
Blake Boucher <blakeboucher@gmail.com>
2016-04-10 14:24:01 -0500

    proper folder hierarchy

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
-var engines = require('consolidate')
+var engines = require('consolidate');
 var MongoClient = require('mongodb').MongoClient;
 var assert = require('assert');
+var crypto = require('crypto');
 
 app.engine('html', engines.handlebars);
 app.set('view engine', 'html');
@@ -21,10 +22,11 @@ app.get('/', function(req, res) {
     var query = {};
     var projection = {"_id": 0, "firstName": 1};
     var cursor = db.collection('users').find(query, projection);
+    cursor.limit(3);
 
     cursor.forEach(
       function(doc) {
-        res.render('home', {'results': doc})
+        res.render('home', {'results': doc});
       }, function(err) {
           assert.equal(err, null);
           console.log("Our query was: " + JSON.stringify(query));
@@ -34,21 +36,75 @@ app.get('/', function(req, res) {
   });
 });
 
+app.post('/login', function(req, res) {
+  MongoClient.connect('mongodb://localhost:27017/bandjo', function(err, db) {
+
+    assert.equal(null, err);
+
+    var email = req.body.email;
+    var password = req.body.password;
+    var count = 0;
+
+    var query = { "email": email };
+    var projection = { "_id": 0, "password": 1 };
+    var cursor = db.collection('users').find(query, projection);
+
+    cursor.forEach(
+      function(doc) {
+        count++;
+      }, function(err) {
+        assert.equal(err, null);
+        console.log("There were " + count + " matches.");
+        if (count==1) {
+          console.log("Found one user with that email.");
+          db.close();
+          if (password==doc.password) {
+            console.log("Successfully logged in " + email + ".");
+            res.redirect("/profile");
+          } else {
+            console.log("Password was incorrect")
+            res.redirect("/");
+          }
+        } else if (count==0) {
+          console.log("No user found with that email.");
+          db.close();
+          res.redirect("/");
+        } else {
+          console.log("What the hell.");
+          db.close();
+          res.redirect("/");
+        }
+      }
+    );
+
+  });
+});
+
+app.get('/profile', function(req, res) {
+  MongoClient.connect('mongodb://localhost:27017/bandjo', function(err, db) {
+
+
+
+  });
+});
+
 app.post('/signup', function(req, res) {
   MongoClient.connect('mongodb://localhost:27017/bandjo', function(err, db) {
 
     assert.equal(null, err);
+
     var firstName = req.body.firstName;
     var lastName = req.body.lastName;
     var email = req.body.email;
     var now = new Date();
-    var password = req.body.password;
+    var password = crypto.createHash("sha1").update(req.body.password).digest('hex');
 
     var query = { "firstName": firstName, "lastName": lastName, "email": email, "joinDate": now, "password": password };
     db.collection('users').insertOne(query, function(err, res) {
       assert.equal(err, null);
       console.log("User successfully created.");
-      return db.close();
+      db.close();
+      res.redirect("/login");
     });
 
   });
